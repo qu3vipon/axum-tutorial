324894b37f78966ddd05ac04f87c8beb68e20358
qu3vipon <sdh5813@gmail.com>
2025-03-07 18:25:39 +0900

    chore: rename cookie

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -2,6 +2,6 @@ pub mod extractor;
 pub mod jwt;
 pub mod middleware;
 
-pub const AUTH_TOKEN: &str = "access-token";
+pub const AUTH_TOKEN: &str = "access_token";
 pub const AUTH_SECRET: &str = "super-secure";
 pub const AUTH_TOKEN_EXPIRY_HOURS: u8 = 24 * 7;
